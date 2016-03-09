8a18e2be2aea875dbbe604b4ce4da7f905a3eb0e
Anndo <handcuffanndo@gmail.com>
2016-03-10 00:19:59 +0800

    Add a new column: password_digest to the users table

--- db/migrate/20160306071001_add_users_table.rb
+++ db/migrate/20160306071001_add_users_table.rb
@@ -2,6 +2,7 @@ class AddUsersTable < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name
+      t.string :password_digest
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20160306071023) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
